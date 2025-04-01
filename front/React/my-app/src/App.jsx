fa55507a25a619b0099845d0d1fd13541ecc4a31
danajlim <danalim0819@gmail.com>
2025-04-01 10:57:14 +0900

    Add: props 매개변수 넘겨주기

--- front/React/my-app/src/App.jsx
+++ front/React/my-app/src/App.jsx
@@ -2,13 +2,26 @@ import { useState } from 'react'
 import './App.css'
 
 //컴포넌트 코드 작성 
-function Header(){
+function Header(props){
+  console.log(props);
   return(
     <header>
-      <h1>WEB</h1>
-      World Wide Web!
+      <h1>{props.title}</h1>
+      {props['desc']}
     </header>
   );
+}
+
+function Nav(props){
+  return(
+    <nav>
+      <ul>
+        <li><a href={props.link1}>{props.text1}</a></li>
+        <li><a href={props.link2}>{props.text2}</a></li>
+        <li><a href={props.link3}>{props.text3}</a></li>
+      </ul>
+    </nav>
+  )
 }
 
 function App() {
@@ -17,7 +30,9 @@ function App() {
   return (
     <>
       <div className="App">
-        <Header></Header>
+        <Header title="WEB" desc={"World Wide Web!"}></Header>
+        <Nav link1="1.html" link2="2.html" link3="3.html" text1="HTML" text2="CSS" text3="JavaScript">
+        </Nav>
       </div>
     </>
   )
